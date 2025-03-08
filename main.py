7e52f61d8ffea7d0165a3db684e5fd5e7b94c5f0
shivamkr-98 <shivamjnv2305@gmail.com>
2025-03-08 17:16:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from pyrogram.errors import FloodWait
 from pyrogram.errors.exceptions.bad_request_400 import StickerEmojiInvalid
 from pyrogram.types.messages_and_media import message
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
+from log import download_logger  # ✅ Import from log.py
 
 bot = Client(
     "bot",
